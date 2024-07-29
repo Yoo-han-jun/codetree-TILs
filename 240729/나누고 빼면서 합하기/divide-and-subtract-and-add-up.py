898fe950e4cdb8fd7d6cd70366200e4ae6b7384f
Yoo-han-jun <66524579+Yoo-han-jun@users.noreply.github.com>
2024-07-29 13:49:11 +0900

    Title: 나누고 빼면서 합하기, Time: 205ms, Memory: 24MB, Status: Passed - CodeTree

--- 240729/나누고 빼면서 합하기/divide-and-subtract-and-add-up.py
+++ 240729/나누고 빼면서 합하기/divide-and-subtract-and-add-up.py
@@ -4,6 +4,9 @@ def s(arr, m):
     sum_val = 0
     while (m>=1):
         sum_val += arr[m-1]
-        m = m//2
+        if m%2==0:
+            m = m//2
+        else:
+            m = m-1
     return sum_val
 print(s(arr, m))
