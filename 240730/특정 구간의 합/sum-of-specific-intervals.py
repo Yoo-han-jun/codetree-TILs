fca6d707bb1c2ac2475303791debb50edb61b44d
Yoo-han-jun <66524579+Yoo-han-jun@users.noreply.github.com>
2024-07-30 11:44:43 +0900

    Title: 특정 구간의 합, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240730/특정 구간의 합/sum-of-specific-intervals.py
@@ -0,0 +1,11 @@
+n, m = map(int,input().split())
+arr = list(map(int,input().split()))
+for _ in range (m):
+    s, b = map(int, input().split())
+    sum_val=0
+    if s==b:
+        print(arr[s])
+    else:
+        for ele in arr[s-1:b]:
+            sum_val+=ele
+        print(sum_val)
