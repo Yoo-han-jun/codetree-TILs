26f8b2f7d5bfb9b054acf3b59570e52f97281c6f
Yoo-han-jun <66524579+Yoo-han-jun@users.noreply.github.com>
2024-07-30 11:45:45 +0900

    Title: 특정 구간의 합, Time: 115ms, Memory: 25MB, Status: Passed - CodeTree

--- 240730/특정 구간의 합/sum-of-specific-intervals.py
+++ 240730/특정 구간의 합/sum-of-specific-intervals.py
@@ -4,7 +4,7 @@ for _ in range (m):
     s, b = map(int, input().split())
     sum_val=0
     if s==b:
-        print(arr[s])
+        print(arr[s-1])
     else:
         for ele in arr[s-1:b]:
             sum_val+=ele
