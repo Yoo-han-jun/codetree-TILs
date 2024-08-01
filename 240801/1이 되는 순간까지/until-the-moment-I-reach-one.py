9884a8ce3dc09ca53e7dcd95d40a38e0d3fb2148
Yoo-han-jun <66524579+Yoo-han-jun@users.noreply.github.com>
2024-08-01 20:49:16 +0900

    Title: 1이 되는 순간까지, Time: 154ms, Memory: 24MB, Status: Passed - CodeTree

--- 240801/1이 되는 순간까지/until-the-moment-I-reach-one.py
+++ 240801/1이 되는 순간까지/until-the-moment-I-reach-one.py
@@ -2,11 +2,12 @@ cnt = 0
 def f(n):
     global cnt
     cnt+=1
-    if n//2 == 1 or n//3 ==1:
+    if n == 1 :
         return cnt
     if n%2==0:
         return f(n//2)
     else:
         return f(n//3)
+        
 n = int(input())
-print(f(n))
+print(f(n)-1)
