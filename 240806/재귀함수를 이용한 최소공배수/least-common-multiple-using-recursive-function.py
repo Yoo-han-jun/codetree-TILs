378818df79d0824970ddaf0e4b660e216f8a0b50
Yoo-han-jun <66524579+Yoo-han-jun@users.noreply.github.com>
2024-08-06 09:49:19 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 123ms, Memory: 24MB, Status: Passed - CodeTree

--- 240806/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
+++ 240806/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
@@ -2,13 +2,9 @@ n = int(input())
 arr = list(map(int, input().split()))
 sum_val = arr[0]
 def low(a, b):
-    cnt = 1
-    if a%b==0 or b%a==0:
-        return min(a,b)
-    for i in range(2, max(a,b)):
-        if a%i==0 and b%i==0:
-            cnt*=i
-    return cnt
+    while b:
+        a, b = b, a % b
+    return a
 
 def s(a, n):
     global sum_val
@@ -16,7 +12,5 @@ def s(a, n):
         return sum_val
     sum_val = (sum_val*a[n-1])//low(sum_val, a[n-1])
     return s(a, n-1)
-
-        
 
 print(s(arr,n))
