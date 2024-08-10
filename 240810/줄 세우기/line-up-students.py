8a58b9618bb03c1256351b2a29ad59c87c4b50fd
Yoo-han-jun <66524579+Yoo-han-jun@users.noreply.github.com>
2024-08-10 18:38:30 +0900

    Title: 줄 세우기, Time: 137ms, Memory: 25MB, Status: Passed - CodeTree

--- 240810/줄 세우기/line-up-students.py
+++ 240810/줄 세우기/line-up-students.py
@@ -1,17 +1,18 @@
 class Student:
-    def __init__(self, height, weight):
+    def __init__(self, height, weight, number):
         self.height = height
         self.weight = weight
+        self.number = number
 
 arr = []
 n = int(input())
-for _ in range(n):
+for i in range(1, n+1):
     he, we = tuple(input().split())
-    stu = Student(int(he),int(we))
+    stu = Student(int(he),int(we), int(i))
     arr.append(stu)
 
-arr.sort(key=lambda x: (x.height, x.weight)) # 국어 점수 기준 내림차순 정렬
+arr.sort(key=lambda x: (-x.height, -x.weight, x.number)) # 국어 점수 기준 내림차순 정렬
 
 # 정렬 이후 등수별 학생 번호 출력
-for idx, stu in reversed(list(enumerate (arr,start=1))):
-    print(f"{stu.height} {stu.weight} {idx}")
+for idx, stu in enumerate (arr,start=1):
+    print(f"{stu.height} {stu.weight} {stu.number}")
