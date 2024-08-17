adb58f7c1d15a6d9a8baf13d30144df475613041
Yoo-han-jun <66524579+Yoo-han-jun@users.noreply.github.com>
2024-08-17 09:52:59 +0900

    Title: 그 요일은, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240817/그 요일은/the-day-of-the-day.py
+++ 240817/그 요일은/the-day-of-the-day.py
@@ -5,11 +5,13 @@ a, b, c, d = map(int,input().split())
 dat = input()
 cnt = 0
 cnt2 = 0
-for _ in range (1, a):
-    cnt += month[a-1]
+for i in range (1, a):
+    cnt += month[i-1]
+cnt+=b
+for i in range (1, c):
+    cnt2 += month[i-1]
+cnt2+=d
 
-for _ in range (1, c):
-    cnt2 += month[c-1]
 if (cnt2-cnt)%7 >= day.index(dat)+1:
     print(((cnt2-cnt)//7)+1)
 else:
