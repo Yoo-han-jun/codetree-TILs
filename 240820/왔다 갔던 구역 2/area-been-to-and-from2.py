66ef8fa4b283e6c8bafdac5ad0bd298a57879994
Yoo-han-jun <66524579+Yoo-han-jun@users.noreply.github.com>
2024-08-20 00:08:54 +0900

    Title: 왔다 갔던 구역 2, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240820/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -0,0 +1,19 @@
+n = int(input())
+cnt = 100
+cnt2 = 0
+arr = [0]*200
+for _ in range (n):
+    a, b = tuple(input().split())
+    a = int(a)
+    if b=="R":
+        for i in range(cnt, cnt+a):
+            arr[i]+=1
+        cnt = cnt+a
+    else:
+        for i in range(cnt,cnt-a,-1):
+            arr[i]+=1
+        cnt = cnt-a
+for ele in arr:
+    if ele>=2:
+        cnt2+=1
+print(cnt2)
