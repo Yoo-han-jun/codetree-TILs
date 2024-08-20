66f26cdec5b8a85b6746a1acb8267fecd93f9b09
Yoo-han-jun <66524579+Yoo-han-jun@users.noreply.github.com>
2024-08-20 20:34:10 +0900

    Title: 흰검 칠하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240820/흰검 칠하기/painting-white-black.py
+++ 240820/흰검 칠하기/painting-white-black.py
@@ -1,7 +1,7 @@
 n = int(input())
-arr1 = [0]*200000
-cnt = 100000
-arr2 = [1]*200000
+arr1 = [0]*200
+cnt = 100
+arr2 = [1]*200
 for _ in range (n):
     a, b= tuple(input().split())
     a = int(a)
@@ -9,18 +9,19 @@ for _ in range (n):
         for i in range (cnt, cnt+a):
             arr1[i] = 1
             arr2[i] *=2
-        cnt = cnt+a
+        cnt = cnt+a-1
     elif b=="L":
-        for i in range (cnt-1, cnt-a-1,-1):
+        for i in range (cnt, cnt-a,-1):
             arr1[i] = -1
             arr2[i] *= 3
-        cnt = cnt-a
+        cnt = cnt-a+1
+
 result = [i*j for i, j in zip(arr1, arr2)]
 w = 0
 b = 0
 g = 0
 for ele in result:
-    if abs(ele)//36:
+    if ele!=0 and abs(ele)%36==0:
         g+=1
     elif ele>0:
         b+=1
