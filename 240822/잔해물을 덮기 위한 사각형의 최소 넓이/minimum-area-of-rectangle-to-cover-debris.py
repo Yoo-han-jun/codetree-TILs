fa645a5f58ba73d8b098293a951481853bcdbf5e
Yoo-han-jun <66524579+Yoo-han-jun@users.noreply.github.com>
2024-08-22 01:50:27 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 314ms, Memory: 104MB, Status: Passed - CodeTree

--- 240822/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
+++ 240822/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -1,6 +1,7 @@
 Max_R = 2001
 os = 1000
-
+arr1 = []
+arr2 = []
 arr = [[0 for _ in range (Max_R)] for _ in range (Max_R)]
 
 for i in range (1,3):
@@ -9,13 +10,19 @@ for i in range (1,3):
     for j in range (x1, x2):
         for k in range (y1, y2):
             arr[j][k] = i
-arr1 = []
-arr2 = [] 
+area = 0
 for i in range (len(arr)):
     for j in range (len(arr[i])):
-        if arr[i][j] == 1:
-            arr1.append(i)
-            arr2.append(j)
-x = max(arr1)- min(arr1)
-y = max(arr2) - min(arr2)
-print((x+1)*(y+1))
+        if arr[i][j] ==1:
+            area+=1 
+if area == 0:
+    print(0)
+else:
+    for i in range (len(arr)):
+        for j in range (len(arr[i])):
+            if arr[i][j] == 1:
+                arr1.append(i)
+                arr2.append(j)
+    x = max(arr1)- min(arr1)
+    y = max(arr2) - min(arr2)
+    print((x+1)*(y+1))
