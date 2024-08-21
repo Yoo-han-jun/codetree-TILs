d3e2a03e9f9fa4b5169b6ab78632d4666b4131c5
Yoo-han-jun <66524579+Yoo-han-jun@users.noreply.github.com>
2024-08-22 02:01:12 +0900

    Title: 계속 중첩되는 사각형, Time: 149ms, Memory: 25MB, Status: Passed - CodeTree

--- 240822/계속 중첩되는 사각형/continuously-overlapping-squares.py
+++ 240822/계속 중첩되는 사각형/continuously-overlapping-squares.py
@@ -1,16 +1,40 @@
-MAX_R = 2001
-os = 1000
-arr = [[0 for _ in range (MAX_R)] for _ in range (MAX_R)]
+MAX_R = 200
+OFFSET = 100
+
+# 변수 선언 및 입력
 n = int(input())
-for i in range (n):
-    x1, y1, x2, y2 = map(int,input().split())
-    x1, y1, x2, y2 = x1+os, y1+os, x2+os, y2+os
-    for k in range(x1, x2):
-        for j in range (y1, y2):
-            arr[k][j] = i
+checked = [
+    [0] * (MAX_R + 1)
+    for _ in range(MAX_R + 1)
+]
+
+x1, y1, x2, y2 = [], [], [], []
+for i in range(n):
+    _x1, _y1, _x2, _y2 = tuple(map(int, input().split()))
+    x1.append(_x1)
+    y1.append(_y1)
+    x2.append(_x2)
+    y2.append(_y2)
+
+    # OFFSET을 더해줍니다.
+    x1[i] += OFFSET
+    y1[i] += OFFSET
+    x2[i] += OFFSET
+    y2[i] += OFFSET
+
+# 직사각형에 주어진 순으로 1, 2 번호를 붙여줍니다.
+# 격자 단위로 진행하는 문제이므로
+# x2, y2에 등호가 들어가지 않음에 유의합니다.
+for i in range(n):
+    for x in range(x1[i], x2[i]):
+        for y in range(y1[i], y2[i]):
+            checked[x][y] = 1 if i % 2 == 0 else 2
+
+# 숫자 2로 남아있는 영역의 넓이를 구합니다.
 area = 0
-for x in range (len(arr)):
-    for y in range (len(arr[i])):
-        if arr[x][y]%2==1:
-            area+=1
+for x in range(MAX_R + 1):
+    for y in range(MAX_R + 1):
+        if checked[x][y] == 2:
+            area += 1
+
 print(area)
