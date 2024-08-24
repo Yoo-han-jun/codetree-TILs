3dd2b07e35389f06daea80e26ed98158f532e7bd
Yoo-han-jun <66524579+Yoo-han-jun@users.noreply.github.com>
2024-08-25 03:14:26 +0900

    Title: 선두를 지켜라, Time: 264ms, Memory: 77MB, Status: Passed - CodeTree

--- 240825/선두를 지켜라/keep-the-lead.py
+++ 240825/선두를 지켜라/keep-the-lead.py
@@ -1,10 +1,11 @@
 n, m = map(int,input().split())
-arr1 = [0]*50
-arr2 = [0]*50
+arr1 = [0]*1000000
+arr2 = [0]*1000000
 num1 = 1
 cnt = 0
 num2 =1
 cnt2 = 0
+
 for _ in range (n):
     v, t = map(int,input().split())
     for _ in range(t):
@@ -17,19 +18,9 @@ for _ in range (m):
         arr2[num2] = arr2[num2-1] + v
         num2+=1
 result = [a - b for a, b in zip(arr1, arr2)]
-cnt2=0
-arr3 = []
-for i in range (cnt):
-    if result[i]<0:
-        arr3.append(-1)
-    if result[i]>0:
-        arr3.append(1)
-    if result[i]==0:
-        arr3.append(0)
-for i in range (cnt):
-    if arr3[i] ==0:
-        arr3[i] = arr3[i-1]
-for i in range (cnt):
-    if arr3[i]!=arr3[i-1]:
+signs = [1 if x > 0 else -1 if x < 0 else 0 for x in result]
+signs2 = [x for x in signs if x != 0]
+for i in range (1,len(signs2)):
+    if signs2[i]!=signs2[i-1]:
         cnt2+=1
 print(cnt2)
