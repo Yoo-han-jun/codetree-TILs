c1f5e91d4c6841b075c0fb63635f299bc987d0bb
Yoo-han-jun <66524579+Yoo-han-jun@users.noreply.github.com>
2024-08-27 20:56:19 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 125ms, Memory: 24MB, Status: Passed - CodeTree

--- 240827/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 240827/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -5,20 +5,21 @@ class ex:
 
 arr = [0]*n
 arr2 = [0]*n
-arr[p] = 1
+arr[p-1] = 1
 arr3 = []
 for _ in range (t):
     t, x, y = tuple(input().split())
     pl = ex(int(t), int(x), int(y))
     arr3.append(pl)
 arr3.sort(lambda x: x.t)
-cnt = 0
 for ele in arr3:
-    arr2[ele.x-1]+=1
-    arr2[ele.y-1]+=1
-    if arr[ele.x-1] ==1 and arr2[ele.x-1]<=k:
-        arr[ele.y-1] =1
-    if arr[ele.y-1] == 1 and arr2[ele.y-1]<=k:
+    if arr[ele.x-1]==1:
+        arr2[ele.x-1]+=1
+    if arr[ele.y-1]==1:
+        arr2[ele.y-1]+=1
+    if (arr[ele.x-1] ==1 and arr2[ele.x-1]<=k) or (arr[ele.y-1] == 1 and arr2[ele.y-1]<=k):
         arr[ele.x-1] =1
+        arr[ele.y-1] =1
+        
 for ele in arr:
     print(ele, end="")
