aaf09a187626f5cb850d34661f6a1494247cb879
Yoo-han-jun <66524579+Yoo-han-jun@users.noreply.github.com>
2024-08-30 22:02:20 +0900

    Title: 되돌아오기, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240830/되돌아오기/come-back.py
@@ -0,0 +1,26 @@
+n = int(input())
+x,y = 0, 0
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
+mapper = {
+    "W":2,
+    "E":0,
+    "N":1,
+    "S":3
+}
+cnt = 0
+answer = False
+for _ in range(n):
+    dir_c, t = tuple(input().split())
+    t = int(t)
+    if answer==True:
+        print(cnt)
+        break
+    for _ in range(t):
+        x, y = x+dx[mapper[dir_c]], y+dy[mapper[dir_c]]
+        cnt += 1
+        if x==0 and y==0 :
+            answer = True
+            break
+if answer == False:
+    print(-1)
