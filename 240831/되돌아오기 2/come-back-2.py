b25e4d58bbc700b698e8a05de2969b080ef2170f
Yoo-han-jun <66524579+Yoo-han-jun@users.noreply.github.com>
2024-08-31 11:56:14 +0900

    Title: 되돌아오기 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240831/되돌아오기 2/come-back-2.py
@@ -0,0 +1,23 @@
+n = tuple(input())
+x, y = 0,0
+dx = [1, 0, -1, 0]
+dy = [0, -1, 0, 1]
+dir_c = 3
+cnt = 0
+for i in range(len(n)):
+    if n[i] =="L":
+        dir_c = (dir_c+3)%4
+        cnt +=1
+    if n[i] =="R":
+        dir_c = (dir_c+1)%4
+        cnt +=1
+    if n[i] =="F":
+        x, y = x+dx[dir_c], y+dy[dir_c]
+        cnt+=1
+    if x==0 and y==0:
+        answer = True
+        break
+if answer == True:
+    print(cnt)
+else:
+    print(-1)
