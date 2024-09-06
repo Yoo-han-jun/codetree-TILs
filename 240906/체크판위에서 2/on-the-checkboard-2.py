b6c3fbec23e9280fb96f069e1a514b4c584f3143
Yoo-han-jun <66524579+Yoo-han-jun@users.noreply.github.com>
2024-09-06 21:28:48 +0900

    Title: 체크판위에서 2, Time: 114ms, Memory: 25MB, Status: Passed - CodeTree

--- 240906/체크판위에서 2/on-the-checkboard-2.py
+++ 240906/체크판위에서 2/on-the-checkboard-2.py
@@ -3,8 +3,7 @@ arr = [list(map(str,input().split())) for _ in range (n)]
 sum_val = 0
 for i in range(1, n-1):
     for j in range (1, m-1):
-        arr[i][j]!=arr[0][0]
-        if arr[i][j] == arr[n-1][m-1]:
+        if arr[i][j]!=arr[0][0] and arr[i][j] == arr[n-1][m-1]:
             for k in range(i+1, n-1):
                 for l in range(j+1, m-1):
                     if arr[k][l] != arr[i][j]:
