004bf523d953acbf8d89598772d314d6604fc303
Yoo-han-jun <66524579+Yoo-han-jun@users.noreply.github.com>
2024-09-06 21:55:36 +0900

    Title: 이상한 진수 2, Time: 107ms, Memory: 25MB, Status: Passed - CodeTree

--- 240906/이상한 진수 2/awkward-digits-2.py
+++ 240906/이상한 진수 2/awkward-digits-2.py
@@ -1,5 +1,9 @@
 n = list(map(int, input()))
-n[n.index(0)] = 1
+
+if 0 in n :
+    n[n.index(0)] = 1
+elif 0 not in n:
+    n[len(n)-1] = 0
 sum_val = 0
 for i in range (len(n)):
     sum_val = (sum_val+n[i])*2
