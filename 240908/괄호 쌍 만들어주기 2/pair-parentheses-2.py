d5bcc165575f316fb79f2ac7f7ae21ddbee05b2e
Yoo-han-jun <66524579+Yoo-han-jun@users.noreply.github.com>
2024-09-08 14:35:33 +0900

    Title: 괄호 쌍 만들어주기 2, Time: 132ms, Memory: 24MB, Status: Passed - CodeTree

--- 240908/괄호 쌍 만들어주기 2/pair-parentheses-2.py
+++ 240908/괄호 쌍 만들어주기 2/pair-parentheses-2.py
@@ -1,6 +1,6 @@
 n = tuple(input())
 cnt = 0
-for i in range (len(n)):
+for i in range (len(n)-1):
     if n[i] =="(" and n[i+1] =="(":
         for k in range (i+2, len(n)-1):
             if n[k] ==")" and n[k+1] ==")":
