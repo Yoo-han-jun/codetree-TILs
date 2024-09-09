7bb6ce1b5a00d019b796402b77a44809b929203b
Yoo-han-jun <66524579+Yoo-han-jun@users.noreply.github.com>
2024-09-09 21:46:06 +0900

    Title: 씨 오 더블유 2, Time: 112ms, Memory: 35MB, Status: Passed - CodeTree

---
+++ 240909/씨 오 더블유 2/c-o-w-2.py
@@ -0,0 +1,11 @@
+n = int(input())
+a = tuple(input())
+cnt = 0
+for i in range (n):
+    if a[i] == "C":
+        for j in range (i+1, n):
+            if a[j] == "O":
+                for k in range (j+1, n):
+                    if a[k] == "W":
+                        cnt+=1
+print(cnt)
