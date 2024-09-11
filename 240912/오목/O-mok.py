3c2e6d784ea6d0ca1d56b163d5dd15c5628bdbcd
Yoo-han-jun <66524579+Yoo-han-jun@users.noreply.github.com>
2024-09-12 02:44:02 +0900

    Title: 오목, Time: 124ms, Memory: 25MB, Status: Passed - CodeTree

--- 240912/오목/O-mok.py
+++ 240912/오목/O-mok.py
@@ -3,18 +3,18 @@ def in_range(x, y):
     return x<19 and x>=0 and y<19 and y>=0
 ans = False
 a_x, a_y = 0, 0
-for i in range(2, 16):
-    for j in range(2,16):
+for i in range(19):
+    for j in range(19):
         cnt1, cnt2, cnt3, cnt4 =0 ,0, 0, 0
         for k in range (-2, 3):
             if arr[i][j] ==1 or arr[i][j]==2:
-                if arr[i][j] == arr[i+k][j-k]:
+                if in_range(i+k, j-k) and arr[i][j] == arr[i+k][j-k]:
                     cnt1 +=1
-                if arr[i][j] == arr[i+k][j]:
+                if in_range(i+k, j) and arr[i][j] == arr[i+k][j]:
                     cnt2 +=1
-                if arr[i][j] == arr[i][j+k]:
+                if in_range(i, j+k) and arr[i][j] == arr[i][j+k]:
                     cnt3 +=1
-                if arr[i][j] == arr[i+k][j+k]:
+                if in_range(i+k, j+k) and arr[i][j] == arr[i+k][j+k]:
                     cnt4 +=1
             if cnt1 ==5 or cnt2 ==5 or cnt3==5 or cnt4==5:
                 a_x, a_y = i,j
