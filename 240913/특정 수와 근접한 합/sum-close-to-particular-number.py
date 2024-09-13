80100d977e1c344b37dc5ef219091413ed03a427
Yoo-han-jun <66524579+Yoo-han-jun@users.noreply.github.com>
2024-09-13 20:08:44 +0900

    Title: 특정 수와 근접한 합, Time: 131ms, Memory: 25MB, Status: Passed - CodeTree

--- 240913/특정 수와 근접한 합/sum-close-to-particular-number.py
+++ 240913/특정 수와 근접한 합/sum-close-to-particular-number.py
@@ -3,9 +3,7 @@ arr = list(map(int,input().split()))
 sum_val = 10000000000
 for i in range (s):
     for j in range(i+1, s):
-        for k in range (j+1, s):
-            for l in range (k+1, s):
-                cnt = arr[i]+arr[j]+arr[k]+arr[l]
-                min_val = abs(cnt-n)
-                sum_val = min(min_val, sum_val)
+        cnt = sum(arr)-arr[i]-arr[j]
+        min_val = abs(cnt-n)
+        sum_val = min(min_val, sum_val)
 print(sum_val)
