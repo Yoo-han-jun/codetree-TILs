a01cf8f13a60acde7ee32e18141d659ddcbd1662
Yoo-han-jun <66524579+Yoo-han-jun@users.noreply.github.com>
2024-09-13 19:10:47 +0900

    Title: 최고의 13위치 2, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240913/최고의 13위치 2/best-place-of-13-2.py
@@ -0,0 +1,30 @@
+n = int(input())
+arr = [list(map(int,input().split())) for _ in range (n)]
+
+def in_range(x,y):
+    return x>=0 and x<n and y>=0 and y<n
+max_val = 0
+for i in range(n):
+    for j in range(n):
+        for k in range(i, n):
+            if i==k:
+                for l in range(j+1, n):
+                    cnt= 0
+                    if in_range(i,j) and in_range(i,j+2) and in_range(k,l) and in_range(k,l+2):
+                        for m in range (3):
+                            if arr[i][j+m]==1:
+                                cnt+=1
+                            if arr[k][l+m]==1:
+                                cnt+=1
+                    max_val = max(max_val, cnt)
+            else:
+                for l in range(n):
+                    cnt= 0
+                    if in_range(i,j) and in_range(i,j+2) and in_range(k,l) and in_range(k,l+2):
+                            for m in range (3):
+                                if arr[i][j+m]==1:
+                                    cnt+=1
+                                if arr[k][l+m]==1:
+                                    cnt+=1
+                    max_val = max(max_val, cnt)
+print(max_val)
