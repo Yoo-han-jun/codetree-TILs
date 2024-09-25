95855c6402736f1a001601792cbefe2bb023f654
Yoo-han-jun <66524579+Yoo-han-jun@users.noreply.github.com>
2024-09-26 01:30:27 +0900

    Title: 바구니 안의 사탕 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240926/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -0,0 +1,16 @@
+n, k = map(int,input().split())
+arr = [0 for _ in range (100)]
+for _ in range(n):
+    a, b = map(int,input().split())
+    arr[b] += a
+sum_val = 0
+for i in range (100):
+    cnt_val = 0
+    if i+k+1>=100:
+        for j in range (max(0, i-k), 100):
+            cnt_val += arr[j]
+    else:
+        for j in range (max(0, i-k), i+k+1):
+            cnt_val += arr[j]
+    sum_val = max(sum_val, cnt_val)
+print(sum_val)
