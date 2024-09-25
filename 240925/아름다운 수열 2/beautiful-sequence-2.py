94c460055051b3c2eb70146554e64e36047b0212
Yoo-han-jun <66524579+Yoo-han-jun@users.noreply.github.com>
2024-09-25 20:27:07 +0900

    Title: 아름다운 수열 2, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240925/아름다운 수열 2/beautiful-sequence-2.py
+++ 240925/아름다운 수열 2/beautiful-sequence-2.py
@@ -5,7 +5,7 @@ sum_val=0
 for i in range (n-m+1):
     arr3 = [0 for _ in range (m)]
     for j in range (i, i+m):
-        for l in range (3):
+        for l in range (m):
             if arr2[l] == arr[j]:
                 arr3[l] = arr2[l]
     if arr3 == arr2:
