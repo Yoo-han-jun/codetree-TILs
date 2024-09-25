faa1671c61dcdcbe4861aedc275d3a5d4efdc7e4
Yoo-han-jun <66524579+Yoo-han-jun@users.noreply.github.com>
2024-09-26 01:34:17 +0900

    Title: 바구니 안의 사탕 2, Time: 111ms, Memory: 28MB, Status: Passed - CodeTree

--- 240926/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 240926/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -1,5 +1,5 @@
 n, k = map(int,input().split())
-arr = [0 for _ in range (100)]
+arr = [0 for _ in range (101)]
 for _ in range(n):
     a, b = map(int,input().split())
     arr[b] += a
@@ -7,7 +7,7 @@ sum_val = 0
 for i in range (100):
     cnt_val = 0
     if i+k+1>=100:
-        for j in range (max(0, i-k), 100):
+        for j in range (max(0, i-k), 101):
             cnt_val += arr[j]
     else:
         for j in range (max(0, i-k), i+k+1):
