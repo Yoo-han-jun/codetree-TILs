91f7b05a42cbdcfb42aa1becd53ec74bea25dba1
Yoo-han-jun <66524579+Yoo-han-jun@users.noreply.github.com>
2024-09-25 20:41:32 +0900

    Title: 아름다운 수열 2, Time: 136ms, Memory: 24MB, Status: Passed - CodeTree

--- 240925/아름다운 수열 2/beautiful-sequence-2.py
+++ 240925/아름다운 수열 2/beautiful-sequence-2.py
@@ -1,15 +1,14 @@
 n, m = map(int, input().split())
 arr = list(map(int,input().split()))
 arr2 = list(map(int,input().split()))
+arr2.sort()
 sum_val=0
 for i in range (n-m+1):
     arr3 = []
     for j in range (i, i+m):
-        for l in range (m):
-            arr3.append(arr[l])
-        arr2.sort()
-        arr3.sort()
-        if arr3 == arr2:
-            sum_val+=1
-        arr3.clear()
-print(sum_val//m)
+        arr3.append(int(arr[j]))
+    arr3.sort()
+    if arr3 == arr2:
+        sum_val+=1
+    arr3.clear()
+print(sum_val)
