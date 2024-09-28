d8f187d25fcdcb15563476e03c8240874af30cf1
Yoo-han-jun <66524579+Yoo-han-jun@users.noreply.github.com>
2024-09-28 22:41:06 +0900

    Title: G or H 2, Time: 147ms, Memory: 25MB, Status: Passed - CodeTree

--- 240928/G or H 2/G-or-H-2.py
+++ 240928/G or H 2/G-or-H-2.py
@@ -17,7 +17,7 @@ for i in range (len(arr2)):
                 cnt_G+=1
             if arr[arr2[k]] == "H":
                 cnt_H+=1
-        if cnt_G == cnt_H:
-            cnt_val = arr2[j]-arr2[i]+1
-    max_val = max(max_val, cnt_val)
+        if cnt_G == cnt_H or cnt_G==0 or cnt_H==0:
+            cnt_val = arr2[j]-arr2[i]
+        max_val = max(max_val, cnt_val)
 print(max_val)
