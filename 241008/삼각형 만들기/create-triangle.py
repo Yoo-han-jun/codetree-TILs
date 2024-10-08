fa7f22c5383682d4f885a99c69bc0373b645a299
Yoo-han-jun <66524579+Yoo-han-jun@users.noreply.github.com>
2024-10-08 19:06:48 +0900

    Title: 삼각형 만들기, Time: 161ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 241008/삼각형 만들기/create-triangle.py
@@ -0,0 +1,14 @@
+n = int(input())
+arr = []
+max_val = 0
+for i in range (n):
+    a, b = tuple(map(int, input().split()))
+    arr.append ((a, b))
+for i in range (n):
+    for j in range (n):
+        for k in range(n):
+            if i==j or j==k or k==i:
+                continue
+            z = abs((arr[i][0]*arr[j][1]+arr[j][0]*arr[k][1]+arr[k][0]*arr[i][1]) - (arr[j][0]*arr[i][1]+arr[k][0]*arr[j][1]+arr[i][0]*arr[k][1]))
+            max_val = max(z, max_val)
+print(max_val)
