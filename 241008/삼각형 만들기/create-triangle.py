e4eced0c9ec7b2b563809f6bec0bee79fa705dbd
Yoo-han-jun <66524579+Yoo-han-jun@users.noreply.github.com>
2024-10-08 19:15:22 +0900

    Title: 삼각형 만들기, Time: 160ms, Memory: 25MB, Status: Passed - CodeTree

--- 241008/삼각형 만들기/create-triangle.py
+++ 241008/삼각형 만들기/create-triangle.py
@@ -9,6 +9,7 @@ for i in range (n):
         for k in range(n):
             if i==j or j==k or k==i:
                 continue
-            z = abs((arr[i][0]*arr[j][1]+arr[j][0]*arr[k][1]+arr[k][0]*arr[i][1]) - (arr[j][0]*arr[i][1]+arr[k][0]*arr[j][1]+arr[i][0]*arr[k][1]))
-            max_val = max(z, max_val)
+            if (arr[i][0] == arr[j][0] and arr[j][1] == arr[k][1]) or (arr[k][0] == arr[j][0] and arr[i][1] == arr[k][1]) or (arr[i][0] == arr[j][0] and arr[j][1] == arr[k][1]):
+                z = abs(arr[i][0]*arr[j][1]+arr[j][0]*arr[k][1]+arr[k][0]*arr[i][1] - arr[j][0]*arr[i][1]-arr[k][0]*arr[j][1]-arr[i][0]*arr[k][1])
+                max_val = max(z, max_val)
 print(max_val)
