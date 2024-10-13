c8df1db68b9d5ae3b5ded1d58797597dc17a1270
Yoo-han-jun <66524579+Yoo-han-jun@users.noreply.github.com>
2024-10-13 18:36:48 +0900

    Title: 스승의 은혜 3, Time: 663ms, Memory: 32MB, Status: Wrong Answer - CodeTree

--- 241013/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 241013/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -1,17 +1,24 @@
 n, b = map(int,input().split())
 arr = [tuple(map(int,input().split())) for _ in range (n)]
-arr.sort(key = lambda x:(x[0]//2+x[1], x[0]+x[1],x[1]))
+arr.sort(key = lambda x:(x[0]//2+x[1],x[0]+x[1],x[1]))
 sum_val = 0
+arr3 = []
 for i in range (n):
-    p, d = arr[i]
-    predict_val = sum_val+p+d
-    if predict_val>= b:
-        predict_val = sum_val + p//2+d
-        if predict_val >=b:
-            print(i)
-            break
-        else:
-            sum_val = predict_val
-    sum_val = predict_val
-    if i == n-1:
-        print(n)
+    arr1 = [0 for _ in range (n)]
+    arr2 = [0 for _ in range (n)]
+    for j in range (i):
+        p, d = arr[j]
+        arr1[j] = int(p)
+        arr2[j] = int(d)
+    predict = sum(arr1)+sum(arr2)
+    if predict>=b:
+        for j in range (i):
+            if arr[j] == max(arr[j]):
+                arr[j] = arr[j]//2
+            predict = sum(arr1)+sum(arr2)
+            if predict>=b:
+                arr3.append(i)
+if arr3:
+    print(min(arr3))
+else:
+    print(n)
