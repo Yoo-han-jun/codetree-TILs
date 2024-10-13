c0b5c5fb2eda724e7415ff1619343bed7e3c2080
Yoo-han-jun <66524579+Yoo-han-jun@users.noreply.github.com>
2024-10-13 14:27:33 +0900

    Title: 선분 3개 지우기, Time: 129ms, Memory: 25MB, Status: Passed - CodeTree

--- 241013/선분 3개 지우기/remove-three-segments.py
+++ 241013/선분 3개 지우기/remove-three-segments.py
@@ -1,6 +1,6 @@
 n = int(input())
 arr = [tuple(map(int,input().split())) for _ in range (n)]
-arr2 = [0 for _ in range (10)]
+arr2 = [0 for _ in range (101)]
 cnt = 0
 union = True
 for i in range(n):
@@ -12,13 +12,13 @@ for i in range(n):
                 pointx1, pointx2 = arr[l]
                 for m in range (pointx1, pointx2+1):
                     arr2[m]+=1
-            for o in range (10):
+            for o in range (101):
                 if arr2[o]>=2:
                     union = False
             if union == True:
                 cnt+=1
-                arr2 = [0 for _ in range (10)]
+                arr2 = [0 for _ in range (101)]
             else:
                 union = True
-                arr2 = [0 for _ in range (10)]
+                arr2 = [0 for _ in range (101)]
 print(cnt)
