7c55ec743c8cde376d4f379ecff3acf3dcb043a0
Yoo-han-jun <66524579+Yoo-han-jun@users.noreply.github.com>
2024-10-13 14:26:35 +0900

    Title: 선분 3개 지우기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241013/선분 3개 지우기/remove-three-segments.py
@@ -0,0 +1,24 @@
+n = int(input())
+arr = [tuple(map(int,input().split())) for _ in range (n)]
+arr2 = [0 for _ in range (10)]
+cnt = 0
+union = True
+for i in range(n):
+    for j in range (i+1, n):
+        for k in range (j+1, n):
+            for l in range(n):
+                if l==j or l==k or l==i:
+                    continue
+                pointx1, pointx2 = arr[l]
+                for m in range (pointx1, pointx2+1):
+                    arr2[m]+=1
+            for o in range (10):
+                if arr2[o]>=2:
+                    union = False
+            if union == True:
+                cnt+=1
+                arr2 = [0 for _ in range (10)]
+            else:
+                union = True
+                arr2 = [0 for _ in range (10)]
+print(cnt)
