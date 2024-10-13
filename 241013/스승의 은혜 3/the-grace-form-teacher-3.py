f724ea84b55b13925a7211da058c79e69a57ba4f
Yoo-han-jun <66524579+Yoo-han-jun@users.noreply.github.com>
2024-10-13 16:03:50 +0900

    Title: 스승의 은혜 3, Time: 118ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241013/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -0,0 +1,12 @@
+n, b = map(int,input().split())
+arr = [tuple(map(int,input().split())) for _ in range (n)]
+arr.sort(key = lambda x:(x[0]+x[1], x[1]))
+sum_val = 0
+for i in range (n):
+    p, d = arr[i]
+    predict_val = sum_val+p+d
+    if predict_val>= b:
+        predict_val = sum_val + p//2+d
+        if predict_val >=b:
+            print(i)
+    sum_val = predict_val
