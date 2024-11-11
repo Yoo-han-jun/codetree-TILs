0a2850aab8dfe29c12483e23297a483a92b07e7c
Yoo-han-jun <66524579+Yoo-han-jun@users.noreply.github.com>
2024-11-11 22:52:43 +0900

    Title: 등차수열, Time: 53ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 241111/등차수열/arithmetic-sequence.py
@@ -0,0 +1,11 @@
+n = int(input())
+arr = tuple(map(int,input().split()))
+max_cnt = 0
+for k in range (min(arr),max(arr)):
+    cnt  = 0
+    for i in range (n):
+        for j in range (i,n):
+            if (arr[i]+arr[j])/2 == k:
+                cnt+=1
+    max_cnt = max(cnt, max_cnt)
+print(max_cnt)
