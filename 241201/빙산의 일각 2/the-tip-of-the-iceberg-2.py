85d03164fcd70ce79d2ccafc5f577b6cdc698efd
Yoo-han-jun <66524579+Yoo-han-jun@users.noreply.github.com>
2024-12-01 20:43:42 +0900

    Title: 빙산의 일각 2, Time: 75ms, Memory: 18MB, Status: Wrong Answer - CodeTree

---
+++ 241201/빙산의 일각 2/the-tip-of-the-iceberg-2.py
@@ -0,0 +1,21 @@
+n = int(input())
+arr = list()
+max_t = 0
+for _ in range (n):
+    arr.append(int(input()))
+for i in range (max(arr)):
+    cnt = list()
+    cnt2 = list()
+    for j in range (n):
+        if arr[j]>=i:
+            cnt.append(1)
+        else:
+            cnt.append(0)
+    for k in range(n-1):
+        if cnt[k] !=cnt [k+1]:
+            cnt2.append(cnt[k])
+        if k==n-2:
+            if cnt[n-1]==1 and cnt[n-2]==0:
+                cnt2.append(cnt[n-1])
+    max_t = max(cnt2.count(1), max_t)
+print(max_t)
