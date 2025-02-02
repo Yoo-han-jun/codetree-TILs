b16ac7f2eb702d04e2584ddeb2382c5ee9939241
Yoo-han-jun <66524579+Yoo-han-jun@users.noreply.github.com>
2025-02-02 20:51:50 +0900

    Title: A, B, C, D 찾기 2, Time: 111ms, Memory: 17MB, Status: Passed - CodeTree

--- 250202/A, B, C, D 찾기 2/find-a-b-c-d-2.py
+++ 250202/A, B, C, D 찾기 2/find-a-b-c-d-2.py
@@ -1,12 +1,13 @@
 nums = list(map(int, input().split()))
 nums.sort()
+answer = False
 # Write your code here!
-for i in range (3, 41):
-    for j in range (3,41):
-        for k in range (4, 41):
-            for l in range (7, 41):
+for i in range (1, 41):
+    for j in range (1,41):
+        for k in range (1, 41):
+            for l in range (1, 41):
                 arr = [i, j, k, l, i+j, j+k, k+l, l+i, i+k, j+l, i+j+k, i+j+l, i+l+k, j+k+l, i+j+k+l]
                 arr.sort()
                 if arr == nums:
                     print(i, j, k, l)
-                    break
+                    exit(0)
