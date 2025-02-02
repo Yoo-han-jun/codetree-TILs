52add1d61888c906fe72b1f89c4d2f4036541d79
Yoo-han-jun <66524579+Yoo-han-jun@users.noreply.github.com>
2025-02-02 21:33:13 +0900

    Title: 독서실의 거리두기 4, Time: 40ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250202/독서실의 거리두기 4/study-cafe-keeping-distance-4.py
@@ -0,0 +1,28 @@
+N = int(input())
+seat = input()
+
+# Write your code here!
+max_dis = 0
+arr2 = []    
+for i,ele in enumerate (seat):
+    if int(ele) == 1:
+        arr2.append(i)
+    
+for i in range (N):
+    for j in range (i+1,N):
+        if i in arr2 or j in arr2:
+            continue
+        arr3 = []
+        for ele in arr2:
+            arr3.append(ele)
+        arr3.append(i)
+        arr3.append(j)
+        arr3.sort()
+        arr4 = []
+        for k in range (len(arr3)-1):
+            dis = arr3[k+1]-arr3[k]
+            arr4.append(dis)
+        if 1 in arr4:
+            continue
+        max_dis = max(max_dis, min(arr4))
+print(max_dis)
