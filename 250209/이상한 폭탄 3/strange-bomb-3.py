036f36948d9541b12f8c4a4d6ad6de0dfe942278
Yoo-han-jun <66524579+Yoo-han-jun@users.noreply.github.com>
2025-02-09 14:24:14 +0900

    Title: 이상한 폭탄 3, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250209/이상한 폭탄 3/strange-bomb-3.py
+++ 250209/이상한 폭탄 3/strange-bomb-3.py
@@ -4,14 +4,19 @@ num = [int(input()) for _ in range(N)]
 # Write
 arr1 = list(set(num))
 arr1.sort()
-arr2=[]
+arr3 = []
 for i in range (len(arr1)):
+    arr2=[]
     cnt = 0
     for j in range (len(num)):
         if arr1[i]==num[j]:
+            arr2.append(j)
+    for k in range (len(arr2)-1):
+        dist = arr2[k+1]-arr2[k]
+        if dist<=K:
             cnt+=1
-    arr2.append(cnt)
-if sum(arr2)==0:
+    arr3.append(cnt)
+if sum(arr3)==0:
     print(0)
 else:
-    print(arr1[arr2.index(max(arr2))])
+    print(arr1[arr3.index(max(arr3))])
