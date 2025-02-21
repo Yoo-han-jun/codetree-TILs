4b29c702aee26fdd43503fa2471d3a6ddf05aa08
Yoo-han-jun <66524579+Yoo-han-jun@users.noreply.github.com>
2025-02-21 11:13:16 +0900

    Title: 구간 잘 나누기, Time: 51ms, Memory: 17MB, Status: Passed - CodeTree

---
+++ 250221/구간 잘 나누기/divide-sections-well.py
@@ -0,0 +1,19 @@
+n, m = map(int, input().split())
+a = list(map(int, input().split()))
+max_num = 100000
+# Write your code here!
+
+for i in range (max(a), max_num):
+    max_cnt = 0
+    s = 1
+    cnt = 0
+    for j in range (len(a)):
+        if cnt+a[j] >i:
+            s +=1
+            cnt = a[j]
+            continue
+        elif cnt+a[j]<=i:
+            cnt+=a[j]
+    if s <= m:
+        print(i)
+        break
