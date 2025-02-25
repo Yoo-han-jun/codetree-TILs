8864041b408e29c743d2146061f739a61d0eebf5
Yoo-han-jun <66524579+Yoo-han-jun@users.noreply.github.com>
2025-02-25 23:51:50 +0900

    Title: 두 직사각형, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

--- 250225/두 직사각형/two-rectangles.py
+++ 250225/두 직사각형/two-rectangles.py
@@ -4,10 +4,10 @@ a1, b1, a2, b2 = map(int, input().split())
 # Write your code here!
 
 def intersect(x1, y1, x2, y2, a1, b1, a2, b2):
-    if x2>=a1 and x2<=a2 and y1>=b1 and y1<=b2:
-        print("overlapping")
-    else:
+    if x2<a1 or a2<x1 or b2<y1 or y2<b1:
         print("nonoverlapping")
+    else:
+        print("overlapping")
 
 intersect(x1,y1,x2,y2,a1,b1,a2,b2)
         
