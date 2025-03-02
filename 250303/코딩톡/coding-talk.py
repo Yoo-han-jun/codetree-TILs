470f59de8e6f3c1d2dc5f16523b028960222a5bb
Yoo-han-jun <66524579+Yoo-han-jun@users.noreply.github.com>
2025-03-03 03:31:00 +0900

    Title: 코딩톡, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 250303/코딩톡/coding-talk.py
+++ 250303/코딩톡/coding-talk.py
@@ -8,6 +8,9 @@ u = [int(msg[1]) for msg in messages]
 l = {idx: 0 for idx in range (n)}
 for i in range (p-1, m):
     l[ord(c[i])-65] +=1
+for j in range (m):
+    if u[j]>=u[p-1]:
+        l[ord(c[j])-65] +=1
 if u[p-1]==0:
     print()
 else:
