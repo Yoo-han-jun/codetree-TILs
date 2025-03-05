bd4313f7be80031a17fc45ad104143090234f540
Yoo-han-jun <66524579+Yoo-han-jun@users.noreply.github.com>
2025-03-05 11:24:22 +0900

    Title: 순 간 이 동, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 250305/순 간 이 동/teleportation.py
+++ 250305/순 간 이 동/teleportation.py
@@ -2,7 +2,7 @@ a, b, x, y = map(int, input().split())
 
 # Please write your code here.
 
-distance = b-a
+distance = abs(b-a)
 distance2 = abs(min(a, b)- min(x,y))+abs(max(a,b)-max(x,y))
 
 distance = min(distance, distance2)
