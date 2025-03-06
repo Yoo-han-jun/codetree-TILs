c8b19fd90b98043bba492374dc46c8572229c1fe
Yoo-han-jun <66524579+Yoo-han-jun@users.noreply.github.com>
2025-03-06 23:46:19 +0900

    Title: 연속된 숫자 만들기 2, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 250306/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
+++ 250306/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
@@ -2,15 +2,16 @@ pos = list(map(int, input().split()))
 cnt = 0
 cnt2 = 0
 # Please write your code here.
-for i in range (2):
-    if abs(pos[i]-pos[i+1])==1:
-        cnt+=1
-    elif abs(pos[i]-pos[i+1])==2:
-        cnt2+=1
+for i in range (3):
+    for j in range(i+1,3):
+        if abs(pos[i]-pos[j])==1:
+            cnt+=1
+        elif abs(pos[i]-pos[j])==2:
+            cnt2+=1
 
-if cnt2>=1:
-    print(1)
-elif cnt==2:
+if cnt==2:
     print(0)
+elif cnt2>=1:
+    print(1)
 else:
     print(2)
