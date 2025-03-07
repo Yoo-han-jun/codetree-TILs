3e1d7643f2a5e3f3a9e21325fc58c316b6f2a02a
Yoo-han-jun <66524579+Yoo-han-jun@users.noreply.github.com>
2025-03-07 23:41:18 +0900

    Title: 독서실의 거리두기 3, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250307/독서실의 거리두기 3/study-cafe-keeping-distance-3.py
@@ -0,0 +1,27 @@
+N = int(input())
+seats = input()
+arr=[]
+arr2=[]
+# Please write your code here.
+for i in range (N):
+    if int(seats[i])==1:
+        arr.append(i)
+for i in range (len(arr)-1):
+    arr2.append(arr[i+1]-arr[i])
+num = arr2.index(max(arr2))
+
+min_cnt = 1000
+for j in range (arr[num]+1, arr[num+1]):
+    arr3 = [j]
+    arr4 = []
+    for i in range (N):
+        if int(seats[i])==1:
+            arr3.append(i)
+    arr3.sort()        
+    for i in range (len(arr3)-1):
+        k = arr3[i+1]-arr3[i]
+        if k==1:
+            continue    
+        arr4.append(k)
+    min_cnt = min(min(arr4), min_cnt)
+print(min_cnt)
