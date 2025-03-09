b0c4d63280bd4c11617f20dc882870b356cea3d1
Yoo-han-jun <66524579+Yoo-han-jun@users.noreply.github.com>
2025-03-09 22:09:23 +0900

    Title: 개발자의 가위바위보, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 250309/개발자의 가위바위보/developers-rock-paper-scissors.py
+++ 250309/개발자의 가위바위보/developers-rock-paper-scissors.py
@@ -3,6 +3,7 @@ moves = [tuple(map(int, input().split())) for _ in range(N)]
 a, b = zip(*moves)
 a, b = list(a), list(b)
 arr = [1, 2, 3]
+arr2 = [1, 3, 2]
 # Please write your code here.
 def win(x, y, a, b, c):
     if x==a and y==b:
@@ -19,7 +20,17 @@ for i in range (3):
     for j in range (N):
         x= a[j]
         y= b[j]
-        e, f, g = arr[i//3], arr[(i+1)//3], arr[(i+2)//3]
+        e, f, g = arr[i%3], arr[(i+1)%3], arr[(i+2)%3]
+        if win(x,y,e,f,g):
+            cnt+=1
+    max_cnt = max(cnt, max_cnt)
+
+for i in range (3):    
+    cnt = 0
+    for j in range (N):
+        x= a[j]
+        y= b[j]
+        e, f, g = arr2[i%3], arr2[(i+1)%3], arr2[(i+2)%3]
         if win(x,y,e,f,g):
             cnt+=1
     max_cnt = max(cnt, max_cnt)
