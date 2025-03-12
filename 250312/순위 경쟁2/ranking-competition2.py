6e6e3decba623159eef73955a1a599df3ef012fa
Yoo-han-jun <66524579+Yoo-han-jun@users.noreply.github.com>
2025-03-12 14:38:53 +0900

    Title: 순위 경쟁2, Time: 49ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250312/순위 경쟁2/ranking-competition2.py
@@ -0,0 +1,36 @@
+n = int(input())
+c = []
+s = []
+for _ in range(n):
+    ci, si = input().split()
+    c.append(ci)
+    s.append(int(si))
+
+# Please write your code here.
+A_s = 0
+B_s = 0
+cnt= 0
+arr = [0, 0, 0]
+for i in range (n):
+    if c[i]=="A":
+        A_s+=s[i]
+    else:
+        B_s+=s[i]
+    arr2 = arr.copy()
+    if A_s>B_s:
+        arr[0] = 1
+        arr[1] = 0
+        arr[2] = 0
+    if A_s<B_s:
+        arr[0] = 0
+        arr[1] = 1
+        arr[2] = 0
+    if A_s==B_s:
+        arr[0] = 0
+        arr[1] = 0
+        arr[2] = 1
+    if arr2!=arr:
+        cnt+=1
+print(cnt)
+
+        
