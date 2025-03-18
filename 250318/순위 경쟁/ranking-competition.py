8fbef59e4081a3df43555f2df3d8013248ae9043
Yoo-han-jun <66524579+Yoo-han-jun@users.noreply.github.com>
2025-03-18 19:42:08 +0900

    Title: 순위 경쟁, Time: 46ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250318/순위 경쟁/ranking-competition.py
@@ -0,0 +1,46 @@
+n = int(input())
+c = []
+s = []
+for _ in range(n):
+    ci, si = input().split()
+    c.append(ci)
+    s.append(int(si))
+
+# Please write your code here.
+def get_score(a1, a2, a3):
+    if a1==a2==a3:
+        return 1
+    if a1==a2 and a1<a3:
+        return 2
+    if a1==a2 and a1>a3:
+        return 3
+    if a2==a3 and a2>a1:
+        return 4
+    if a2==a3 and a2<a1:
+        return 5
+    if a1==a3 and a3<a2:
+        return 6
+    if a1==a3 and a3>a2:
+        return 7
+    if a1!=a2 and a2!=a3 and a3!=a2:
+        if max(a1, a2, a3)==a1:
+            return 8
+        if max(a1, a2, a3)==a2:
+            return 9
+        if max(a1, a2, a3)==a3:
+            return 10
+arr = [0, 0, 0]
+state = 1
+cnt = 0
+for i in range (n):
+    state = get_score(arr[0], arr[1], arr[2])
+    if c[i] =="A":
+        arr[0]+=s[i]
+    if c[i]=="B":
+        arr[1]+=s[i]
+    if c[i]=="C":
+        arr[2]+=s[i]
+    if state != get_score(arr[0], arr[1], arr[2]):
+        cnt+=1
+
+print(cnt)
