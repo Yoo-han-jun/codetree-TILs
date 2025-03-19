efc1e49715352a6daf491234ab8e93a0e66f5b5e
Yoo-han-jun <66524579+Yoo-han-jun@users.noreply.github.com>
2025-03-19 10:59:28 +0900

    Title: 세 수의 최대 곱, Time: 41ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250319/세 수의 최대 곱/maximum-product-of-three-numbers.py
+++ 250319/세 수의 최대 곱/maximum-product-of-three-numbers.py
@@ -1,11 +1,40 @@
 n = int(input())
 arr = list(map(int, input().split()))
-
+arr1 = []
+arr2 = []
+arr3 = []
 # Please write your code here.
-cnt = -10000000
 for i in range (n):
-    for j in range (i+1, n):
-        for k in range (j+1, n):
-            cbt = arr[i] * arr[j] * arr[k]
-        cnt = max(cnt, cbt)
-print(cnt)
+    if arr[i]>0:
+        arr1.append(arr[i])
+    if arr[i]<0:
+        arr2.append(arr[i])
+    if arr[i]==0:
+        arr3.append(arr[i])
+arr1.sort()
+arr1.reverse()
+arr2.sort()
+arr3.sort()
+cnt = 0
+if n==3:
+    print(arr[1]*arr[0]*arr[2])
+elif len(arr2)>=2 and len(arr1)>=2:
+    cnt = max(arr2[0]*arr1[0]*arr1[1], arr2[0]*arr2[1]*arr1[0])
+elif len(arr2)>=2 and len(arr1)==1:
+    cnt = arr2[0]*arr2[1]*arr1[0]
+elif len(arr2)>=2 and len(arr1)==0:
+    arr2.reverse()
+    cnt = arr2[0]*arr2[1]*arr1[2]
+elif len(arr2)==1 and len(arr1)==2:
+    arr1.reverse()
+    cnt = arr2[0]*arr1[0]*arr1[1]
+elif len(arr2)<=1 and len(arr1)>=3:
+    cnt = arr1[2]*arr1[0]*arr1[1]
+elif len(arr2)<2 and len(arr1)<2:
+    print(0)
+
+if cnt<0 and len(arr3)!=0:
+    print(0)
+else:
+    print(cnt)
+
