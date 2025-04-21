28ea028f62fdb1880a592b2133b0523ea83688df
Yoo-han-jun <66524579+Yoo-han-jun@users.noreply.github.com>
2025-04-21 23:12:49 +0900

    Title: 최고의 33위치, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250421/최고의 33위치/best-place-of-33.py
+++ 250421/최고의 33위치/best-place-of-33.py
@@ -1,13 +1,13 @@
 n = int(input())
 grid = [list(map(int, input().split())) for _ in range(n)]
-cnt = 0
+max_cnt = 0
 # Please write your code here.
 for i in range(n-2):
     for j in range (n-2):
         cnt = 0
         for k in range (i, i+3):
             for l in range (j, j+3):
-                if gird[k][l]==1:
+                if grid[k][l]==1:
                     cnt+=1
         max_cnt = max(max_cnt, cnt)
 
