0ce93d7c9d5f5f7851e1ab2e17d796e02ec3ab6c
Yoo-han-jun <66524579+Yoo-han-jun@users.noreply.github.com>
2025-04-23 22:52:25 +0900

    Title: 행복한 수열의 개수, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250423/행복한 수열의 개수/number-of-happy-sequence.py
+++ 250423/행복한 수열의 개수/number-of-happy-sequence.py
@@ -2,29 +2,29 @@ n, m = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
 max_num = 0
 # Please write your code here.
-def find(arr, n, m, max_num):
+def find(arr, n, m, d):
     for i in range (n):
         cnt = 1
         n = arr[i][0]
-        for j in range (1,n-1):
+        for j in range (1,n):
             if n==arr[i][j]:
                 cnt+=1
                 if cnt>=m:
-                    max_num+=1
+                    d+=1
                     continue
             else:
                 n=arr[i][j]
                 cnt = 1
 
-def find2(arr, n, m, max_num):
+def find2(arr, n, m, d):
     for i in range (n):
         cnt = 1
         n = arr[0][i]
-        for j in range (1,n-1):
+        for j in range (1,n):
             if n==arr[j][i]:
                 cnt+=1
                 if cnt>=m:
-                    max_num+=1
+                    d+=1
                     continue
             else:
                 n=arr[j][i]
